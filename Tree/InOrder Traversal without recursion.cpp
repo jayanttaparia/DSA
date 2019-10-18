7452f656ab09cd60630a2dddf4c597e7d6840fc2
jayanttaparia <37138009+jayanttaparia@users.noreply.github.com>
2019-10-18 11:47:10 +0530

    InOrder Traversal without recursion

---
+++ Tree/InOrder Traversal without recursion.cpp
@@ -0,0 +1,34 @@
+/**
+ * Definition for binary tree
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+ 
+ TreeNode* insert(TreeNode* cur, stack<TreeNode*>&stk){
+     while(cur!=NULL){
+        stk.push(cur);
+        cur = cur->left;
+    }
+    return cur;
+ }
+ 
+vector<int> Solution::inorderTraversal(TreeNode* A) {
+    vector<int> ans;
+    stack<TreeNode*> stk;
+    TreeNode *cur = A;
+    cur = insert(cur, stk);
+    
+    while(cur == NULL && !stk.empty()){
+        cur = stk.top();
+        stk.pop();
+        ans.push_back(cur->val);
+        cur=cur->right;
+        cur = insert(cur, stk);
+    }
+    
+    return ans;
+}
